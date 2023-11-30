d54bca665f0f886b5d97ac301ebe127cad3b906c
Lazarus <27408@ma-web.nl>
2023-11-30 14:28:53 +0100

    ver4.1

--- 02 canvashuis/app.js
+++ 02 canvashuis/app.js
@@ -5,7 +5,13 @@ class App
         console.log("hello world")
         let canvas = document.getElementById("canvasid")
         let g = canvas.getContext("2d");
+
+        g.fillStyle = "red";
+        g.fillRect(0,0,canvas.width,canvas.height);
+
+        g.fillStyle = "black";
         g.fillRect(0,0,10,10);
+
         console.log(canvas)
     }
 }
