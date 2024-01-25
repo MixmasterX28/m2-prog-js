78ae88d11dca8b71e90b81a6a0f8a7dfd7215155
Lazarus <27408@ma-web.nl>
2024-01-25 15:33:00 +0100

    5.2 done

--- 05 array/app.js
+++ 05 array/app.js
@@ -12,7 +12,38 @@ class App
             for (let i = 0; i < array.length; i++){
                 const element = array[i];
                 console.log(i + ": " + element);
+
             }
+
+            array.push("Dylan bob", "prince")
+
+            for (let i = 0; i < array.length; i++){
+                const element = array[i];
+                console.log(i + ": " + element);
+
+            }
+
+            array.pop()
+            array.pop()
+
+            for (let i = 0; i < array.length; i++){
+                const element = array[i];
+                console.log(i + ": " + element);
+
+            }
+
+            array.push("NAOKI", "Mili")
+
+            for (let i = 0; i < array.length; i++){
+                const element = array[i];
+                console.log(i + ": " + element);
+
+            }
+
+
+
+
+
 
             for (let i = 0; i < array.length; i++){
                 const element = array2[i];
