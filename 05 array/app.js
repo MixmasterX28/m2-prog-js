1b00e5c9888683d56c9e02d4a4fb38118f9e66d9
Lazarus <27408@ma-web.nl>
2024-01-30 14:28:27 +0100

    5.3 af

--- 05 array/app.js
+++ 05 array/app.js
@@ -42,14 +42,33 @@ class App
 
 
 
-
-
-
             for (let i = 0; i < array.length; i++){
                 const element = array2[i];
                 const bigmath = element + 1;
                 console.log(bigmath)
             }
+
+            let headers = document.getElementsByTagName("h1")
+
+            for (let i = 0; i < array.length; i++){
+                const element = headers[i];
+                console.log(element);
+
+            }
+
+            let headersclass = document.getElementsByClassName("bandname");
+
+            for (let i = 0; i < array.length; i++){
+                const element = headersclass[i];
+                console.log(element.innerText = "Huh???");
+
+            }
+
+            headersclass[0].innerText = "nieuws";
+            headersclass[1].innerText = "reviews";
+            headersclass[2].innerText = "commentaar"
+            headersclass[3].innerText = "Ballin"
+            headersclass[4].innerText = "Rock"
         }
 }
 
